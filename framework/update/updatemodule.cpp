8504b9a8f71d90abc182a8c6cc77f5eb3ffff211
Eism <eismailzada@gmail.com>
2026-09-08 14:54:24 +0300

    copy helper's logs from ProgramData next to app's logs on windows
    The task runs as SYSTEM and logs into the secured %ProgramData% root it owns, mirror that file next to the application logs.

--- framework/update/updatemodule.cpp
+++ framework/update/updatemodule.cpp
@@ -95,6 +95,10 @@ void UpdateModule::resolveImports()
 void UpdateModule::onInit(const IApplication::RunMode&)
 {
     m_configuration->init();
+
+#if defined(Q_OS_WIN)
+    std::static_pointer_cast<WinUpdateInstaller>(m_updateInstaller)->importHelperLog();
+#endif
 }
 
 IContextSetup* UpdateModule::newContext(const muse::modularity::ContextPtr& ctx) const
